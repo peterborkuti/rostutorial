860bac2a7985e561f28c99a092344db6f1b8d954
Peter Borkuti <peterborkuti@gmail.com>
2021-04-18 18:03:46 +0200

    cpp publisher

---
+++ my_cpp_pkg/src/my_first_publisher.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+class MyFirstPublisher : public rclcpp::Node
+{
+public:
+    MyFirstPublisher() : Node("cpp_publisher"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
+
+        publisher = this->create_publisher<example_interfaces::msg::String>("string_messages", 10);
+
+        timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                         std::bind(&MyFirstPublisher::timerCallback, this));
+    }
+
+private:
+    
+    rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher;
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+        auto data = example_interfaces::msg::String();
+        data.data = "Hello " + counter_;
+        publisher->publish(data);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MyFirstPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
